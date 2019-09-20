ce7648fe7c4761376423b01348a49419d6928bf1
Scott Ratigan <Scott@HoneCaps-MacBook-Pro.local>
2019-09-19 17:24:19 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const port = 2019;
+
+app.get("*", (req, res) => {
+  console.log("get request received...");
+  console.log("req.query:", req.query);
+  res.send("response");
+});
+
+app.post("*", (req, res) => {
+  console.log("post request received...");
+  console.log("req.body:", req.body);
+  res.send("response...");
+});
+
+app.listen(port, err => {
+  if (err) return console.error("error starting server:", err);
+  console.log("server started on port", port);
+});
