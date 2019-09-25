f3019235854385a13dcd2756db69e9349063d699
Scott Ratigan <Scott@HoneCaps-MacBook-Pro.local>
2019-09-25 15:29:52 -0700

    another test

--- test.js
+++ test.js
@@ -1,38 +1,14 @@
-// Initialize using signing secret from environment variables
-const { createMessageAdapter } = require("@slack/interactive-messages");
-const slackInteractions = createMessageAdapter(
-  "xoxb-633436176323-766450205172-gVxUpsSsfjdKwewaKobQEDwB"
-);
+const { createEventAdapter } = require("@slack/events-api");
+const slackSigningSecret = "72db46fce7af069fb4e612b00c6ca2b7";
+const slackEvents = createEventAdapter(slackSigningSecret);
+
+// Read the port from the environment variables, fallback to 3000 default.
 const port = process.env.PORT || 3000;
 
-// Handle interactions from messages with a `callback_id` of `welcome_button`
-slackInteractions.action("welcome_button", (payload, respond) => {
-  // `payload` contains information about the action
-  // see: https://api.slack.com/docs/interactive-message-field-guide#action_url_invocation_payload
-  console.log(payload);
+(async () => {
+  // Start the built-in server
+  const server = await slackEvents.start(port);
 
-  // `respond` is a function that can be used to follow up on the action with a message
-  respond({
-    text: "Success!"
-  });
-
-  // The return value is used to update the message where the action occurred immediately.
-  // Use this to items like buttons and menus that you only want a user to interact with once.
-  return {
-    text: "Processing..."
-  };
-});
-
-// Handle interactions from messages containing an action block with an `action_id` of `select_coffee`
-slackInteractions.action({ actionId: "select_coffee" }, (payload, respond) => {
-  // `payload` contains information about the action
-  // Block Kit Builder can be used to explore the payload shape for various action blocks:
-  // https://api.slack.com/tools/block-kit-builder
-  // `respond` and return value are the same as above.
-});
-
-// Start the built-in HTTP server
-slackInteractions.start(port).then(() => {
-  // Listening on path '/slack/actions' by default
-  console.log(`server listening on port ${port}`);
-});
+  // Log a message when the server is ready
+  console.log(`Listening for events on ${server.address().port}`);
+})();
