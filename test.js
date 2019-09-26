0125ec9bc7261a2460aa54e75f5e14373fc9dd45
Scott Ratigan <Scott@HoneCaps-MacBook-Pro.local>
2019-09-26 10:53:38 -0700

    another test - 2

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const { createEventAdapter } = require("@slack/events-api");
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
-const slackEvents = createEventAdapter(slackSigningSecret);
 
 // Read the port from the environment variables, fallback to 3000 default.
 const port = process.env.PORT || 3000;
