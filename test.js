513424e09dd7720935b72706dc8ff14c86d1c77f
Scott Ratigan <Scott@HoneCaps-MacBook-Pro.local>
2019-09-26 10:53:07 -0700

    another test

--- test.js
+++ test.js
@@ -6,22 +6,30 @@ const slackEvents = createEventAdapter(slackSigningSecret);
 // Read the port from the environment variables, fallback to 3000 default.
 const port = process.env.PORT || 3000;
 
-// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
-slackEvents.on("message", event => {
+// Initialize the adapter to trigger listeners with envelope data and headers
+const slackEvents = createEventAdapter(slackSigningSecret, {
+  includeBody: true,
+  includeHeaders: true
+});
+
+// Listeners now receive 3 arguments
+slackEvents.on("message", (event, body, headers) => {
   console.log(
     `Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`
   );
-});
-
-slackEvents.on("error", error => {
-  console.log(JSON.stringify(error));
-  console.log(error.name); // TypeError
+  console.log(
+    `The event ID is ${body.event_id} and time is ${body.event_time}`
+  );
+  if (headers["X-Slack-Retry-Num"] !== undefined) {
+    console.log(
+      `The delivery of this event was retried ${
+        headers["X-Slack-Retry-Num"]
+      } times because ${headers["X-Slack-Retry-Reason"]}`
+    );
+  }
 });
 
 (async () => {
-  // Start the built-in server
   const server = await slackEvents.start(port);
-
-  // Log a message when the server is ready
   console.log(`Listening for events on ${server.address().port}`);
 })();
